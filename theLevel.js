eb08e4cdb00710c7af1a7ec5d8a5e7c7ab3e8013
Travis Simpson <travis.simpson92@hotmail.com>
2019-01-12 17:36:25 -0500

    'styling'

--- theLevel.js
+++ theLevel.js
@@ -1,3 +1,6 @@
+// dependencies
+
+import "./theLevel.css"
 /*----------------------------------------Basic Level Design--------------------------------- */
 let simpleLevelPlan = `
 ......................
@@ -134,4 +137,90 @@ const levelChars = {
 
 /*----------------------------------------Create The Level--------------------------------- */
 let simpleLevel = new Level(simpleLevelPlan);
-console.log(`${simpleLevel.width} by ${simpleLevel.height}`);
+// console.log(`${simpleLevel.width} by ${simpleLevel.height}`);
+
+/*-----------------------------------------Function For Elements w/Attributes + Child Nodes--------------------------------------------------------*/
+
+function elt(name, attrs, ...children) {
+    let dom = document.createElement(name);
+    for (let attr of Object.keys(attrs)) {
+        dom.setAttribute(attr, attrs[attr]);
+    }
+    for (let child of children) {
+        dom.appendChild(child);
+    }
+    return dom;
+}
+
+/*----------------------------------------Creates A Display------------------------------------*/
+class DOMDisplay {
+    constructor(parent, level) {
+        this.dom = elt("div", {class: "game"}, drawGrid(level));
+        this.actorLayer = null;
+        parent.appendChild(this.dom);
+    }
+
+    clear() { this.dom.remove(); }
+}
+
+// BG Grid is drawn once, as it never changes. Actors are redrawn every time the display is updated with a given state.
+
+function drawGrid(level) {
+    return elt("table", {
+        class: "background",
+        style: `width: ${level.width * scale}px`
+    }, ...level.rows.map(row =>
+        elt("tr", {style: `height: ${scale}px`},
+            ...row.map(type => elt("td", {class: type})))
+    ));
+}
+
+// The spread operator is used to pass arrays of child nodes to elt as separate arguments
+
+/*-----------------------------------Draw each actor--------------------------*/
+
+function drawActors(actors) {
+    return elt("div", {}, ...actors.map(actor=> {
+        let rect = elt("div", {class: `actor ${actor.type}`});
+        rect.style.width = `${actor.size.x * scale}px`;
+        rect.style.height = `${actor.size.y * scale}px`;
+        rect.style.left = `${actor.pos.x * scale}px`;
+        rect.style.top = `${actor.pos.y * scale}px`;
+        return rect;
+    }));
+}
+
+/*-----------------------------------syncState is used to make the display show a given state.it Removes old actor graphics and redraws new positions-------*/
+DOMDisplay.prototype.syncState = function(state) {
+    if (this.actorLayer) this.actorLayer.remove();
+    this.actor = drawActors(state.actors);
+    this.dom.appendChild(this.actorLayer);
+    this.dom.className = `game ${state.status}`;
+    this.scrollPlayerIntoView(state);
+};
+
+/*------Scroll Player Into View, incase of overflow---------*/
+DOMDisplay.prototype.scrollPlayerIntoView = function(state) {
+    let width = this.dom.clientWidth;
+    let height = this.dom.clientHeight;
+    let margin = width / 3;
+
+    // The Viewport
+    let left = this.dom.scrollLeft, right = left + width;
+    let top = this.dom.scrollTop, bottom = top + height;
+
+    let player = state.player;
+    let center = player.pos.plus(player.size.times(0.5))
+                            .times(scale);
+    
+    if (center.x < left + margin) {
+        this.dom.scrollLeft = center.x - margin;
+    } else if (center.x > right - margin) {
+        this.dom.scrollLeft = center.x + margin - width;
+    }
+    if (center.y < top + margin) {
+        this.dom.scrollTop = center.y - margin;
+    } else if (center.y > bottom - margin) {
+        this.dom.scrollTop = center.y + margin - height;
+    }
+};
